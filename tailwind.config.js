520b5bc0e1b4dc75c38658e0ab86d91c081179b9
ahmed101essam <ahmed10pepeto@gmail.com>
2025-11-24 16:56:12 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './**/*.liquid'
+  ],
+    theme: {
+        extend:{
+            fontFamily: {
+                bangers: ['Bangers', 'cursive'],
+                poppins:["Poppins", "sans-serif"]
+            }
+        }
+    },
+  safelist: [
+    'bg-[#cd0102]',
+    'bg-[#f2bb06]',
+    'bg-[#ff7e06]',
+    'bg-[#400afe]',
+  ]
+}
